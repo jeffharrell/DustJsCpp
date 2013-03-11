422c133c0f73d038a9aa70b13c0c0903f6c4bf47
Jeff Harrell <jeff@juxtadesign.com>
2013-03-10 22:37:40 -0700

    Fixes to the header file

--- dustjs.cpp
+++ dustjs.cpp
@@ -14,115 +14,93 @@ using namespace std;
 using namespace v8;
 
 
-class DustJs {
+int DustJs::render(const string filename, const map<string, string> &model) {
+	const string tmpl = filename + DUST_EXT;
 
-	public:
-		static int render(const string filename, const map<string, string> &model) {
-			const string tmpl = filename + DUST_EXT;
+	// Create a stack-allocated handle scope
+	HandleScope handle_scope;
 
-			// Create a stack-allocated handle scope
-			HandleScope handle_scope;
+	// Parse the model into a native JSON object
+	Handle<ObjectTemplate> json = ObjectTemplate::New();
 
-			// Parse the model into a native JSON object
-			Handle<ObjectTemplate> json = ObjectTemplate::New();
+    map<string, string>::const_iterator iter;
 
-		    map<string, string>::const_iterator iter;
-
-			for (iter = model.begin(); iter != model.end(); iter++) {
-		    	json->Set(String::New(iter->first.c_str()), String::New(iter->second.c_str()));
-			}
-
-			// Populate the global scope
-			Handle<ObjectTemplate> global = ObjectTemplate::New();
-
-			global->Set(String::New("callback"), FunctionTemplate::New(DustJs::onRender));
-			global->Set(String::New("template"), String::New(filename.c_str()));
-			global->Set(String::New("model"), json);
-
-			// Create a new context
-			Persistent<Context> context = Context::New(NULL, global);
-
-			// Enter the created context
-			Context::Scope context_scope(context);
-
-			// Create a string containing the JavaScript source code
-			Handle<String> dust_source = load(DUST_JS);
-			Handle<String> tmpl_source = load(tmpl);
-			Handle<String> render_source = String::New(DUST_RENDER);
-
-			// Compile the source code
-			Handle<Script> dust_script = Script::Compile(dust_source);
-			Handle<Script> tmpl_script = Script::Compile(tmpl_source);
-			Handle<Script> render_script = Script::Compile(render_source);
-
-			// Run the script to get the result
-			Handle<Value> dust_result = dust_script->Run();
-			Handle<Value> tmpl_result = tmpl_script->Run();
-			Handle<Value> render_result = render_script->Run();
-
-			return 0;
-		}
-
-
-		static int compile (std::string filename) {
-			// Unimplemented
-			return 1;
-		}
-
-
-	protected:
-		static Handle<Value> onRender(const Arguments &args) {
-			String::Utf8Value err(args[0]);
-			String::Utf8Value data(args[1]);
-
-			if (data.length() > 0) {
-				printf("%s\n", *data);
-			} else {
-				printf("%s\n", *err);
-			}
-
-			return Undefined();
-		}
-
-
-	private:
-		// Reads a file into a v8 string
-		static Handle<String> load(const string &name) {
-			FILE* file = fopen(name.c_str(), "rb");
-			if (file == NULL) return Handle<String>();
-
-			fseek(file, 0, SEEK_END);
-			int size = ftell(file);
-			rewind(file);
-
-			char* chars = new char[size + 1];
-			chars[size] = '\0';
-
-			for (int i = 0; i < size;) {
-				int read = fread(&chars[i], 1, size - i, file);
-				i += read;
-			}
-
-			fclose(file);
-			Handle<String> result = String::New(chars, size);
-
-			delete[] chars;
-			return result;
-		}
-
-};
-
-
-int main(int argc, char **argv) {
-	if (argc < 2) {
-		printf("Usage: %s template \n", argv[0]);
-		return 1;
+	for (iter = model.begin(); iter != model.end(); iter++) {
+    	json->Set(String::New(iter->first.c_str()), String::New(iter->second.c_str()));
 	}
 
-	map<string, string> model;
+	// Populate the global scope
+	Handle<ObjectTemplate> global = ObjectTemplate::New();
 
-	model["name"] = "Jeff Harrell";
-	model["count"] = "30";
+	global->Set(String::New("callback"), FunctionTemplate::New(DustJs::onRender));
+	global->Set(String::New("template"), String::New(filename.c_str()));
+	global->Set(String::New("model"), json);
 
-	return DustJs::render("sample/template", model);
+	// Create a new context
+	Persistent<Context> context = Context::New(NULL, global);
+
+	// Enter the created context
+	Context::Scope context_scope(context);
+
+	// Create a string containing the JavaScript source code
+	Handle<String> dust_source = load(DUST_JS);
+	Handle<String> tmpl_source = load(tmpl);
+	Handle<String> render_source = String::New(DUST_RENDER);
+
+	// Compile the source code
+	Handle<Script> dust_script = Script::Compile(dust_source);
+	Handle<Script> tmpl_script = Script::Compile(tmpl_source);
+	Handle<Script> render_script = Script::Compile(render_source);
+
+	// Run the script to get the result
+	Handle<Value> dust_result = dust_script->Run();
+	Handle<Value> tmpl_result = tmpl_script->Run();
+	Handle<Value> render_result = render_script->Run();
+
+	return 0;
+}
+
+
+int DustJs::compile (const string filename) {
+	// Unimplemented
+	return 1;
+}
+
+
+Handle<Value> DustJs::onRender(const Arguments &args) {
+	String::Utf8Value err(args[0]);
+	String::Utf8Value data(args[1]);
+
+	if (data.length() > 0) {
+		printf("%s\n", *data);
+	} else {
+		printf("%s\n", *err);
+	}
+
+	return Undefined();
+}
+
+
+// Reads a file into a v8 string
+Handle<String> DustJs::load(const string &name) {
+	FILE* file = fopen(name.c_str(), "rb");
+	if (file == NULL) return Handle<String>();
+
+	fseek(file, 0, SEEK_END);
+	int size = ftell(file);
+	rewind(file);
+
+	char* chars = new char[size + 1];
+	chars[size] = '\0';
+
+	for (int i = 0; i < size;) {
+		int read = fread(&chars[i], 1, size - i, file);
+		i += read;
+	}
+
+	fclose(file);
+	Handle<String> result = String::New(chars, size);
+
+	delete[] chars;
+	return result;
 }
--- dustjs.h
+++ dustjs.h
@@ -1,8 +1,22 @@
 #ifndef __DUSTJS_H__
 #define __DUSTJS_H__
 
-int render(std::string filename, std::map<std::string, std::string> model);
+#include "lib/v8/include/v8.h"
 
-int compile(std::string filename);
+
+class DustJs {
+
+	public:
+		int render(const std::string filename, const std::map<std::string, std::string> &model);
+		int compile(const std::string filename);
+
+	protected:
+		static v8::Handle<v8::Value> onRender(const v8::Arguments &args);
+
+	private:
+		static v8::Handle<v8::String> load(const std::string &name);
+
+};
+
 
 #endif
